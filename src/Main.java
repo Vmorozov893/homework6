357418f583ec63695efac0856225e24a54203859
vmorozov893 <vmorozov893@gmail.com>
2024-07-27 23:19:52 +0300

    1 задание

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,15 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello and welcome!");
+        System.out.println("Задание 1\n");
+
+        int sum = 0;
+        int i = 1;
+        while (sum < 2_459_000){
+            sum += 15_000;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + sum + " рублей.");
+            i++;
+        }
 
     }
 }
